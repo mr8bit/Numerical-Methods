08cfd0559592236105f032fe7cabee9311cefdc4
cyber <cyber@localhost.localdomain>
2017-04-12 23:16:01 +0300

    Переделал Ньютона

--- Laboratory_work_3/newtons.m
+++ Laboratory_work_3/newtons.m
@@ -1,26 +1,25 @@
-function [x1] = newtons (a, b,eps)
-  x0 = a;
-  if ( our_function(x0) * ddf(x0) > 0)
-    while(1)
+function [x0] = newtons (a, b,eps)
+  x0=a;
+  if ( our_function(x0)*ddf(x0) > eps)
+    while 1
       x1 = x0 - our_function(x0)/df(x0);
-      if (abs(x1-x0) < eps) 
-        x0=x1;
+      if (abs(x1-x0) < eps)
+        display(x1);
+        break;
       else 
-        break
-      endif
+        x0=x1;
+      end
     end
-  x0 = b;
+   end
   if (our_function(x0)*df(x0) > 0)
-    while(1)
+    while 1
       x1 = x0 - our_function(x0)/df(x0);
-      if (abs(x1-x0) < eps) 
-        x0=x1;
+      if (abs(x1-x0) < eps)
+        display(x1);
+        break;
       else 
-        break
-      endif
+        x0=x1;
+      end
     end
-  else 
-    display("Поменяйте начальное приблежение")
   end
-  
 end
